ef97c9ef134362d33298457eb0a72728ba598318
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-23 19:31:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-    lowest_value = nil
-    name_hash.each do |k, v|
-      if lowest_value == nil || v < lowest_value
-        lowest_value = v
-        lowest_key = k
-      end
+  lowest_value = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+       lowest_value = v
+       lowest_key = k
     end
-    lowest_key
+  end
+  lowest_key
 end
