0dbc12c0f6e81b853c27dc474e95c783146a7901
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-23 19:34:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
+#name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   lowest_key = nil
@@ -9,7 +9,6 @@ def key_for_min_value(name_hash)
      if lowest_value == nil || v < lowest_value
        lowest_value = v
        lowest_key = k
-       binding.pry
      end
     end
    lowest_key
